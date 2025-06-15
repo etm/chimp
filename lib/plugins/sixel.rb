a5c1d3dcb630bf880ea7d9ea6546c3ce164687b4
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2025-06-15 18:40:38 +0200

    sixel svg support

--- lib/plugins/sixel.rb
+++ lib/plugins/sixel.rb
@@ -6,7 +6,12 @@ module Chimp
           screen.print 'Use ```sixel,NUM with NUM beeing the width if the picture in characters.'
           return
         end
-        printit = "img2sixel -w #{parameters[0].to_i*10} #{File.join(File.dirname(additional[:fname]),what)}"
+
+        printit = if what =~ /\.svg$/
+          "inkscape -D -d 300 --export-type=png #{File.join(File.dirname(additional[:fname]),what)} -o - | img2sixel -w #{parameters[0].to_i*10}"
+        else
+          "img2sixel -w #{parameters[0].to_i*10} #{File.join(File.dirname(additional[:fname]),what)}"
+        end
         if parameters.include? 'right'
           pos = screen.get_pos
           len = screen.columns
